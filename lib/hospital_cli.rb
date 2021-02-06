5e70a14a80b93e64b8c5e5d20004a6cdddeb53fa
ashg858 <aguidry27598@gmail.com>
2021-02-06 00:01:32 -0600

    first commit

---
+++ lib/hospital_cli.rb
@@ -0,0 +1,10 @@
+require "pry" 
+require "hospital_cli/version"
+#require all files here
+## require all toolsets/libraries
+
+
+module HospitalCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
